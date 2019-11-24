8492dc4e494d1d6d0a8a9b56e86d7f02e1cdf22c
Sandman105 <cpossandon@gmail.com>
2019-11-23 22:12:47 -0500

    Updates

---
+++ precache-manifest.84987fd35bbe188d0365f1640e9c3a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b873ce23087b5fdd9a6ee63e8145a1c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7263525869aac45e7085",
+    "url": "/clicky-game/static/css/main.68ec7669.chunk.css"
+  },
+  {
+    "revision": "552425aac2557157f180",
+    "url": "/clicky-game/static/js/2.2aa87232.chunk.js"
+  },
+  {
+    "revision": "7263525869aac45e7085",
+    "url": "/clicky-game/static/js/main.c5ff0d5d.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  }
+]);
